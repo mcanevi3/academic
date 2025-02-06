21ce4d0648667044d3edc46b42ac35bddb2ebd47
Mehmet Canevi <m.canevi3@gmail.com>
2025-02-06 16:39:06 +0300

    cont'd

--- run1.m
+++ run1.m
@@ -40,19 +40,19 @@ plot(t,y,'LineWidth',2,'DisplayName',num2str(x0'));
 
 print("plot1.png","-dpng","-r150")
 
-% tvec=0:0.1:100;
-% sys=ss(A,B,eye(2),zeros(2,1));
+tvec=0:0.1:100;
+sys=ss(A+B*L,B,eye(2),zeros(2,1));
 
-% figure(2);clf;hold on;grid minor;
-% xlabel("x_1");xlabel("x_2");title("Phase portrait");
-% for x0_1=-1000:100:1000
-%     for x0_2=-1000:100:1000
-%         [x,t]=initial(sys,[x0_1,x0_2]);
-%         plot(x(1,:),x(2,:),'k');
-%         drawnow;
-%     end
-% end
-sympref("FloatingPointOutput",true);
+figure(2);clf;hold on;grid minor;
+xlabel("x_1");xlabel("x_2");title("Phase portrait");
+for x0_1=-1000:100:1000
+    for x0_2=-1000:100:1000
+        [x,t]=initial(sys,[x0_1,x0_2]);
+        plot(x(1,:),x(2,:),'k--');
+        drawnow;
+    end
+end
+
 syms t;
 Phi=expm((A+B*L)*t);
 Phi=simplify(rewrite(Phi,"sin"));
@@ -72,14 +72,37 @@ disp(fliplr(coeffs(Phi(2,2))))
 
 syms x10 x20 real
 xt=Phi*[x10;x20];
-ut=L*xt;
-ut=simplify(rewrite(ut,"sin"));
-disp("u:")
+xt=simplify(rewrite(xt,"sin"));
+% disp("x:");
 
-temp=subs(ut,x20,0);
-disp(vpa(temp,4))
-disp(fliplr(coeffs(temp)))
+% temp=subs(xt(2),x20,0);
+% disp(vpa(temp,4))
+% disp(fliplr(coeffs(temp)))
+% temp=subs(xt(2),x10,0);
+% disp(vpa(temp,4))
+% disp(fliplr(coeffs(temp)))
+fxt=matlabFunction(xt);
+fxt=@(t)-0.0045*cos(0.0473*t)+0.0045*sin(0.0473*t);
+figure(2);clf;hold on;grid minor;
+tvec=0:0.1:100;
+plot(tvec,fxt(tvec),'LineWidth',2)
 
-temp=subs(ut,x10,0);
-disp(vpa(temp,4))
-disp(fliplr(coeffs(temp)))
+ulim=[0.0064,0.0946]*[10;10]
+
+x01inf=inv(0.0064);
+x02inf=inv(0.0946);
+
+x0eq=x01inf*x02inf/(x01inf+x02inf);
+x0eq
+% return
+% ut=L*xt;
+% ut=simplify(rewrite(ut,"sin"));
+% disp("u:")
+
+% temp=subs(ut,x20,0);
+% disp(vpa(temp,4))
+% disp(fliplr(coeffs(temp)))
+
+% temp=subs(ut,x10,0);
+% disp(vpa(temp,4))
+% disp(fliplr(coeffs(temp)))
